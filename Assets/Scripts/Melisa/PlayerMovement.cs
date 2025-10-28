bdb83bfb9de2e3813137ccde59a242a4063bf3c2
Melisa Huerta <melisahuerta0806@gmail.com>
2025-10-28 18:14:46 -0500

    Melisa - PlayerMovement

---
+++ Assets/Scripts/Melisa/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 4f;
+    private Rigidbody2D playerRb;
+    private Animator playerAnimator;
+    public Vector2 MoveInput { get; private set; }
+    public Vector2 LastMoveDirection { get; private set; }
+    public bool CanMove { get; set; } = true;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (!CanMove) return;
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        MoveInput = input.normalized;
+
+        if (MoveInput != Vector2.zero)
+            LastMoveDirection = MoveInput;
+
+        if (input != Vector2.zero)
+        {
+            playerAnimator.SetFloat("Horizontal", input.x);
+            playerAnimator.SetFloat("Vertical", input.y);
+        }
+
+        playerAnimator.SetFloat("Speed", MoveInput.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!CanMove) return;
+
+        playerRb.MovePosition(playerRb.position + MoveInput * speed * Time.fixedDeltaTime);
+    }
+}
