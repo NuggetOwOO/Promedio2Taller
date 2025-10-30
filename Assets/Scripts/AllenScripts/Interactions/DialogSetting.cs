905bf64bbe9fa631566bd7be2390b718bc66b42e
Allen <allencitoowo@gmail.com>
2025-10-29 20:06:11 -0500

    dialog script edit

--- Assets/Scripts/AllenScripts/Interactions/DialogSetting.cs
+++ Assets/Scripts/AllenScripts/Interactions/DialogSetting.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DialogSetting", menuName = "Game/DialogSetting")]
 public class DialogSetting : ScriptableObject
 {
-    public float charactersPerSecond;
-    public float turboCharactersPerSecond;
+    public float charactersPerSecond = 20f;
+    public float turboCharactersPerSecond = 100f;
     public Sprite character1Default;
     public Sprite character2Default;
     public List<Dialog> dialogs;
